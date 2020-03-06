abf0adca7a02513bb7f9c6f4cae75dbcc56f6868
PedrinhowVieira <pedro.dmvieira@gmail.com>
2020-03-05 22:20:18 -0300

    add square validate matrix

--- src/echo/echo.go
+++ src/echo/echo.go
@@ -3,12 +3,16 @@ package echo
 import (
 	"fmt"
 	"strings"
+	"validate"
 )
 
 // Echo returns the matrix as a string in matrix format.
-func Echo(records [][]string) string {
+func Echo(matrix [][]string) string {
+	if validate.SquareMatrix(matrix) != nil {
+		return validate.StringNonSquareMatrix + "\n"
+	}
 	var response string
-	for _, row := range records {
+	for _, row := range matrix {
 		response = fmt.Sprintf("%s%s\n", response, strings.Join(row, ","))
 	}
 	return response
---
+++ src/validate/validate_test.go
@@ -0,0 +1,45 @@
+package validate
+
+import (
+	"testing"
+)
+
+func TestSquareMatrix(t *testing.T) {
+	t.Run("return error if number of cols are greater than rows", func(t *testing.T) {
+		matrix := [][]string{
+			{"1","2","3"},
+			{"4","5","6"},
+		}
+		err := SquareMatrix(matrix)
+		assertError(t, err, ErrNonSquareMatrix)
+	})
+	t.Run("return error if number of rows are greater than cols", func(t *testing.T) {
+		matrix := [][]string{
+			{"1","2"},
+			{"3","4"},
+			{"5","6"},
+		}
+		err := SquareMatrix(matrix)
+		assertError(t, err, ErrNonSquareMatrix)
+	})
+	t.Run("do nothing if the number of rows and cols are the same", func(t *testing.T) {
+		matrix := [][]string{
+			{"1","2"},
+			{"3","4"},
+		}
+		err := SquareMatrix(matrix)
+		if err != nil {
+			t.Errorf("should not returned an error but it did")
+		}
+	})
+}
+
+func assertError(t *testing.T, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
